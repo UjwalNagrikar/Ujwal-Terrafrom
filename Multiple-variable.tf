d9e95f71ebc92bcf77554698b8a9aff7f3ee4729
Ujwal Nagrikar <98095028+UjwalNagrikar@users.noreply.github.com>
2024-12-16 21:46:40 +0530

    Add files via upload

---
+++ Multiple-variable.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  type = string
+  default = "Ujwal"
+}
+
+variable "age" {
+  type = number
+  default = 19
+}
+
+variable "DOB" {
+  type = string
+  default = "30/11/2005"
+}
+
+output "name" {
+  value = "Hello, ${var.name}"
+}
+
+output "age" {
+  value = "You are ${var.age} years old"
+}
+
+output "DOB" {
+  value = "Your date of birth is ${var.DOB}"
+}
